b59382281ba107b685d7c6bd0733544b1b412aec
arunomkari <102797605+arunomkari@users.noreply.github.com>
2026-04-21 14:46:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,43 +1,65 @@
 resource "aws_launch_template" "web_server_as" {
-    name = "myproject"
-    image_id           = "ami-0e12ffc2dd465f6e4"
-    vpc_security_group_ids = [aws_security_group.web_server.id]
-    instance_type = "t3.micro"
-    key_name = "Arun_key"
-    tags = {
-        Name = "DevOps"
-    }
-    
+  name = "myproject"
+
+  image_id      = "ami-0e12ffc2dd465f6e4"
+  instance_type = "t3.micro"
+  key_name      = "Arun_key"
+
+  vpc_security_group_ids = [
+    aws_security_group.web_server.id
+  ]
+
+  tags = {
+    Name = "DevOps"
+  }
 }
    
 
 
-  resource "aws_elb" "web_server_lb"{
-     name = "web-server-lb"
-     security_groups = [aws_security_group.web_server.id]
-     subnets = ["subnet-02fc6aa492fa01293", "subnet-08b9c82044e995962"]
-     listener {
-      instance_port     = 8000
-      instance_protocol = "http"
-      lb_port           = 80
-      lb_protocol       = "http"
-    }
-    tags = {
-      Name = "terraform-elb"
-    }
-  }
-resource "aws_autoscaling_group" "web_server_asg" {
-    name                 = "web-server-asg"
-    min_size             = 1
-    max_size             = 3
-    desired_capacity     = 2
-    health_check_type    = "EC2"
-    load_balancers       = [aws_elb.web_server_lb.name] 
-    launch_template {
-        id      = aws_launch_template.web_server_as.id
-        version = "$Latest"
-      }
-    
-    
+ resource "aws_elb" "web_server_lb" {
+  name            = "web-server-lb"
+  security_groups = [aws_security_group.web_server.id]
+
+  subnets = [
+    "subnet-02fc6aa492fa01293",
+    "subnet-08b9c82044e995962"
+  ]
+
+  listener {
+    instance_port     = 8000
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
   }
 
+  tags = {
+    Name = "terraform-elb"
+  }
+}
+
+resource "aws_autoscaling_group" "web_server_asg" {
+  name             = "web-server-asg"
+  min_size         = 1
+  max_size         = 3
+  desired_capacity = 2
+
+  # ✅ REQUIRED: Tell ASG where to launch instances
+  vpc_zone_identifier = [
+    "subnet-02fc6aa492fa01293",
+    "subnet-08b9c82044e995962"
+  ]
+
+  launch_template {
+    id      = aws_launch_template.web_server_as.id
+    version = "$Latest"
+  }
+
+  load_balancers    = [aws_elb.web_server_lb.name]
+  health_check_type = "EC2"
+
+  tag {
+    key                 = "Name"
+    value               = "web-server"
+    propagate_at_launch = true
+  }
+}
